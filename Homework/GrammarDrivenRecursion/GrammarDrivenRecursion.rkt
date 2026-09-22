83140b1815adf9f7b43e1fec83b98d869e981883
Amelia Kremer <kremerar@rose-hulman.edu>
2026-09-22 11:15:34 -0400

    hello

--- Homework/GrammarDrivenRecursion/GrammarDrivenRecursion.rkt
+++ Homework/GrammarDrivenRecursion/GrammarDrivenRecursion.rkt
@@ -2,33 +2,66 @@
 
 (provide slist-map slist-reverse slist-paren-count slist-depth slist-symbols-at-depth path-to make-c...r)
 
+;s-list grammar
+;<s-list>       ::= ( {<s-expression>}* )
+;<s-expression> ::=  <symbol> | <s-list>
+
 (define slist-map
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? b) '()]
+      [(symbol? (car b)) (cons (a (car b)) (slist-map a (cdr b)))]
+      [else (cons (slist-map a (car b)) (slist-map a (cdr b)))])))
 
 (define slist-reverse
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? a) '()]
+      [(symbol? (car a)) (append (slist-reverse (cdr a)) (list (car a)))]
+      [else (append (slist-reverse (cdr a)) (list (slist-reverse (car a))))])))
 
 (define slist-paren-count
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? a) 2]
+      [(symbol? (car a)) (slist-paren-count (cdr a))]
+      [else (+ (slist-paren-count (car a)) (slist-paren-count (cdr a)))])))
 
 (define slist-depth
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? a) 1]
+      [(symbol? (car a)) (slist-depth (cdr a))]
+      [else (max (+ 1 (slist-depth (car a))) (slist-depth (cdr a)))])))
 
 (define slist-symbols-at-depth
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? a) '()]
+      [(symbol? (car a))
+       (if (= b 1) (cons (car a) (slist-symbols-at-depth (cdr a) b)) (slist-symbols-at-depth (cdr a) b))]
+      [else (append (slist-symbols-at-depth (car a) (- b 1)) (slist-symbols-at-depth (cdr a) b))])))
 
 (define path-to
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? a) #f]
+      [(symbol? (car a))
+       (if (eq? (car a) b)
+           '(car)
+           (if (path-to (cdr a) b)
+               (cons 'cdr (path-to (cdr a) b))
+               #f))]
+      [else (if (path-to (car a) b)
+           (cons 'car (path-to (car a) b))
+           (if (path-to (cdr a) b)
+               (cons 'cdr (path-to (cdr a) b))
+               #f))])))
 
 (define make-c...r
   (lambda (str)
-    (nyi)))
+    (apply compose
+    (map (lambda (ch) (if (char=? ch #\a) car cdr)) (string->list str)))))
 
 ;;--------  Used by the testing mechanism   ------------------
 
--- Homework/StatefulClosures/StatefulClosures.rkt
+++ Homework/StatefulClosures/StatefulClosures.rkt
@@ -2,9 +2,44 @@
 
 (provide make-slist-leaf-iterator subst-leftmost)
 
+(define make-stack
+  (lambda ()
+    (let ([stk '()])
+      (lambda (msg . args)
+        (case msg
+          [(empty?) (null? stk)]
+          [(push) (set! stk (cons (car args) stk))]
+          [(pop) (let ([top (car stk)])
+                   (set! stk (cdr stk))
+                   top)]
+          [else
+           (error 'stack "illegal message to stack object:" msg)])))))
+
 (define make-slist-leaf-iterator
-  (lambda (a)
-    (nyi)))
+  (lambda (slist)
+    (let ([stk (make-stack)])
+      (stk 'push slist)
+      (lambda (msg)
+        (let loop ()
+          (if (stk 'empty?)
+              #f
+              (let ([x (stk 'pop)])
+                (cond
+                  [(null? x)
+                   (loop)]
+
+                  [(null? (car x))
+                   (stk 'push (cdr x))
+                   (loop)]
+
+                  [(pair? (car x))
+                   (stk 'push (cdr x))
+                   (stk 'push (car x))
+                   (loop)]
+
+                  [else
+                   (stk 'push (cdr x))
+                   (car x)]))))))))
 
 (define subst-leftmost
   (lambda (a b c d)
